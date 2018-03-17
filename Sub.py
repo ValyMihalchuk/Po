c5c8545b6ef51482b8ac73e78d4e85828ff338a1
Sasha <sas140008@gmail.com>
2018-03-17 22:43:54 +0300

    Update Sub.py

--- Sub.py
+++ Sub.py
@@ -4,18 +4,23 @@ from telegram.ext import Updater, CommandHandler
 import logging
 import os
 
-TOKEN = '487702351:AAF9ZT5I_YCcw7xnju2QLVuW694VZ9f5F70'
+TOKEN = "487702351:AAF9ZT5I_YCcw7xnju2QLVuW694VZ9f5F70"
 PORT = int(os.environ.get('PORT', '8443'))
-updater = Updater(TOKEN)
 
+updater = Updater(TOKEN)
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Hi, pussies!')
-
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
 
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
