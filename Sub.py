1f418625172b3be167f34a07842deab737f1db17
Sasha <sas140008@gmail.com>
2018-03-17 21:39:35 +0300

    Update Sub.py

--- Sub.py
+++ Sub.py
@@ -2,10 +2,12 @@
 
 from telegram.ext import Updater, CommandHandler
 import logging
+import os
 
-token = '487702351:AAF9ZT5I_YCcw7xnju2QLVuW694VZ9f5F70'
+TOKEN = '487702351:AAF9ZT5I_YCcw7xnju2QLVuW694VZ9f5F70'
+PORT = int(os.environ.get('PORT', '8443'))
+updater = Updater(TOKEN)
 
-updater = Updater(token=token)
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -15,4 +17,8 @@ def start(bot, update):
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-updater.start_polling()
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.set_webhook("https://poison-.herokuapp.com/" + TOKEN)
+updater.idle()
