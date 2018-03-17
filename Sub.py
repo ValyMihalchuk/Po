8b86f87574798fdcc79d86f97b129176e74b73ac
Sasha <sas140008@gmail.com>
2018-03-18 00:54:16 +0300

    Update Sub.py

--- Sub.py
+++ Sub.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from telegram.ext import Updater, CommandHandler
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from datetime import date, datetime
 import logging
 import os
 
@@ -11,16 +13,56 @@ updater = Updater(TOKEN)
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Hi, pussies!')
+    greeting = update.message.chat.username if update.message.chat.type in {'private'} else 'pussies'
+    bot.send_message(chat_id=update.message.chat_id, text='Hi, {}!\nI`m here to help you to know '
+                     'the sub of the day, cause I can print it (/sub)'.format(greeting))
+
+
+def help(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Oh, I see, you need some help in using me.\n'
+                                                          'Brilliant! Here are the things I can:\n{}'.format(
+        ''.join([line for line in open('CommandForBot.txt', 'r', encoding='utf-8')])))
+
+
+def sub(bot, update):
+    subs = [sub[:-1:] for sub in open('WeekOfSubs.txt', 'r', encoding='utf-8')]
+    bot.send_message(chat_id=update.message.chat_id, text='And the sub of the day for today is:\n' + subs[date.weekday(date.today())])
+
+
+def time(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=datetime.today().strftime('%H:%M\n%d %h %Y'))
+
+
+def unknown(bot, update):
+    greeting = update.message.chat.username if update.message.chat.type in {'private'} else 'Babes'
+    bot.send_message(chat_id=update.message.chat_id, text='{}, it`s cool, you`re writing me, but I '
+                     'can`t do the thing you want me to do :C\nUse /help for more details'.format(greeting))
+
+
+def text(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Oops! I`m not programmed to "touch" your text, sorry about it!')
+
+
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-#def echo(bot, update):
-#    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+help_handler = CommandHandler('help', help)
+dispatcher.add_handler(help_handler)
 
-#echo_handler = MessageHandler(Filters.text, echo)
-#dispatcher.add_handler(echo_handler)
+sub_handler = CommandHandler('sub', sub)
+dispatcher.add_handler(sub_handler)
+
+time_handler = CommandHandler('time', time)
+dispatcher.add_handler(time_handler)
+
+text_handler = MessageHandler(Filters.text, text)
+dispatcher.add_handler(text_handler)
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
 
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
